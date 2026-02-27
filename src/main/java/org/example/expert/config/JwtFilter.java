4d21ba6f88594c052f390bb7865b237250a4f554
곽현민 <hunmin111@gmail.com>
2026-02-27 19:47:34 +0900

    level: 2-4-a. add spring security dependency and security filter chain

--- src/main/java/org/example/expert/config/JwtFilter.java
+++ src/main/java/org/example/expert/config/JwtFilter.java
@@ -11,11 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
+@Component
 public class JwtFilter implements Filter {
 
     private final JwtUtil jwtUtil;

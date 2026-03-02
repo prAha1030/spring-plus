dea34e4e355a0265be9cb5df22e588a83cc1c339
곽현민 <hunmin111@gmail.com>
2026-03-02 17:02:29 +0900

    level: 2-4-d. update JwtFilter to store authentication in SecurityContext

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,9 +2,15 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
-public class AuthUser {
+public class AuthUser implements UserDetails {
 
     private final Long id;
     private final String email;
@@ -16,5 +22,20 @@ public class AuthUser {
         this.email = email;
         this.userRole = userRole;
         this.nickname = nickname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }
